b26e657262e370760c2c784b16364852d4337eb7
Damian <damitom@o2.pl>
2022-06-18 12:54:16 +0200

    Fixed Engine OCR

--- program/OCR/RecognizeWord.py
+++ program/OCR/RecognizeWord.py
@@ -19,14 +19,14 @@ spell = SpellChecker()
 
 
 
-punclist='.?:;"'
+punclist='.?,'
 punclist2="-+/()[]!`,|*&^%$#@'"
 
 
 smallletters=string.ascii_lowercase
 capitalletters=string.ascii_uppercase
 digits=string.digits
-char_list=smallletters+capitalletters+digits+punclist+punclist2#+'`'	#dla interpunkcji
+char_list=smallletters+capitalletters+digits+punclist#+punclist2#+'`'	#dla interpunkcji
 #char_list = string.ascii_letters+string.digits		#dla podstawowego
 
 
@@ -36,9 +36,9 @@ char_list=smallletters+capitalletters+digits+punclist+punclist2#+'`'	#dla interp
 
 inputs = Input(shape=(32,128,1))
  
-
+# convolution layer with kernel size (3,3)
 conv_1 = Conv2D(64, (3,3), activation = 'relu', padding='same')(inputs)
-
+# poolig layer with kernel size (2,2)
 pool_1 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_1)
  
 conv_2 = Conv2D(128, (3,3), activation = 'relu', padding='same')(pool_1)
@@ -47,34 +47,35 @@ pool_2 = MaxPool2D(pool_size=(2, 2), strides=2)(conv_2)
 conv_3 = Conv2D(256, (3,3), activation = 'relu', padding='same')(pool_2)
  
 conv_4 = Conv2D(256, (3,3), activation = 'relu', padding='same')(conv_3)
-
+# poolig layer with kernel size (2,1)
 pool_4 = MaxPool2D(pool_size=(2, 1))(conv_4)
  
 conv_5 = Conv2D(512, (3,3), activation = 'relu', padding='same')(pool_4)
-
+# Batch normalization layer
 batch_norm_5 = BatchNormalization()(conv_5)
  
 conv_6 = Conv2D(512, (3,3), activation = 'relu', padding='same')(batch_norm_5)
 batch_norm_6 = BatchNormalization()(conv_6)
 pool_6 = MaxPool2D(pool_size=(2, 1))(batch_norm_6)
+
  
 conv_7 = Conv2D(512, (2,2), activation = 'relu')(pool_6)
  
 squeezed = Lambda(lambda x: K.squeeze(x, 1))(conv_7)
- 
 
+# bidirectional LSTM layers with units=128
 blstm_1 = Bidirectional(LSTM(128, return_sequences=True, dropout = 0.2))(squeezed)
 blstm_2 = Bidirectional(LSTM(128, return_sequences=True, dropout = 0.2))(blstm_1)
  
 outputs = Dense(len(char_list)+1, activation = 'softmax')(blstm_2)
 
-
+# model to be used at test time
 act_model = Model(inputs, outputs)
 
 
 
 
-act_model.load_weights('wszystkie_gen2.hdf5') # wszystkie znaki bez przecinka , nie działa wypisuje SSSSS jako odstepy
+act_model.load_weights('wszystkie_duze_male98.hdf5') # wszystkie znaki bez przecinka , nie działa wypisuje SSSSS jako odstepy
 #act_model.load_weights('CRNN_model_downloaded.hdf5')# male litery podstawowy pobrany
 #act_model.load_weights('my_CRNN_model.hdf5')# male litery podstawowy przeszkolony samemu, tutaj powiela  eee na końcu wyrazów
 #act_model.load_weights('myn_best_model.hdf5')# male litery podstawowy przeszkolony samemu, tutaj działa
